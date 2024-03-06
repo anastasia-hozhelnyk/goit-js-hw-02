9af2431e42d7174d1f6d0b070bc182b4f4ae7d95
anastasia-hozhelnyk <151409606+anastasia-hozhelnyk@users.noreply.github.com>
2024-03-07 00:34:30 +0200

    updated

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,8 @@
 function checkForSpam(message) {
-  const lowerCaseMessage = message.toLowerCase();
-  if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
-    return true;
-  } else {
-    return false;
-  }
+  const normalizedMessage = message.toNormalizedMessage();
+  return (
+    normalizedMessage.includes('spam') || normalizedMessage.includes('sale')
+  );
 }
 
 console.log(checkForSpam('Latest technology news')); // false
